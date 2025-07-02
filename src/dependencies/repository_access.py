17a09c7aa3f6006b382c22754eeee6a72736d268
Michael-Jimenez-C <michaeljimenezcontreras1@gmail.com>
2025-07-02 18:25:49 -0500

    refactor: Se han reestructurado el endpoint de asignaturas para utilizar el repositorio

--- src/dependencies/repository_access.py
+++ src/dependencies/repository_access.py
@@ -1,5 +1,9 @@
 from db.engine import engine
 from db.repository.profesor_repository import ProfesorRepository
+from db.repository.asignaturas_repository import AsignaturasRepository
 
 def get_profesor_repository():
     return ProfesorRepository(engine)
+
+def get_asignaturas_repository():
+    return AsignaturasRepository(engine)
