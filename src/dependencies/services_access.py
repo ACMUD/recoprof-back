e7803fcf231c2d4e82d71dd6e24d6893828220b2
Michael-Jimenez-C <michaeljimenezcontreras1@gmail.com>
2025-07-18 18:18:53 -0500

    feat: comments_service añadido como dependencia

--- src/dependencies/services_access.py
+++ src/dependencies/services_access.py
@@ -1,7 +1,12 @@
-from db.engine import get_engine
+from db.engine import get_engine_context
 from services.comments_service import CommentsService
+from db.repository.profesor_repository import ProfesorRepository
+from db.repository.comentarios_repository import ComentarioRepository
+from db.repository.notas_repository import NotasRepository
 
 async def get_comments_service():
-    engine = await get_engine()
-    # Usar modo legacy con engine por simplicidad
-    return CommentsService(comentarios_repo=None, engine=engine)
+    async with get_engine_context() as engine:
+        comentarios_repo: ComentarioRepository = ComentarioRepository(engine)
+        repo_notas: NotasRepository = NotasRepository(engine)
+        repo_profesor: ProfesorRepository = ProfesorRepository(engine)
+        return CommentsService(comentarios_repo=comentarios_repo, repo_notas=repo_notas, repo_profesor=repo_profesor)
