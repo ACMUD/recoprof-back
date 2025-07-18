e7757d59190807148b771013f3971f987eae678a
Michael-Jimenez-C <michaeljimenezcontreras1@gmail.com>
2025-07-18 18:35:20 -0500

    refactor: added type hints and improved error handling in ProfesorRepository methods

--- src/db/repository/profesor_repository.py
+++ src/db/repository/profesor_repository.py
@@ -2,12 +2,13 @@ from odmantic import AIOEngine
 from db.models import Profesor, Notas
 from db.pipelines import paginacion, match, LOOKUP_PIPE, NOTAS, NOTAS_PROMEDIO
 from validations.Values import FacultadesValidas
+from typing import Optional
 
 class ProfesorRepository:
     def __init__(self, engine: AIOEngine):
         self.engine = engine
 
-    async def get_profesor(self, profesor_id):
+    async def get_profesor(self, profesor_id) -> Profesor | None:
         """
         Obtiene la entidad Profesor por su ID.
         """
@@ -25,7 +26,7 @@ class ProfesorRepository:
         if len(profesor) == 0:
             return None
         return profesor[0]
-    
+
     async def get_profesor_list(self, page: int = 0, limit: int = 10, name: str = '', facultad: FacultadesValidas = None):
         name = name.upper()
         pipeline = []
@@ -40,15 +41,27 @@ class ProfesorRepository:
 
         return profesores
 
-    async def count_profesor_list(self, name: str = '', facultad: FacultadesValidas = None):
+    async def count_profesor_list(self, name: str = '', facultad: Optional[FacultadesValidas] = None):
         name = name.upper()
-        total = total = await self.engine.count(Profesor, Profesor.nombre.match("[A-z0-9 ]*"+name+"[A-z0-9 ]*"),Profesor.facultades == facultad if facultad else {})
+        total: int = await self.engine.count(Profesor, Profesor.nombre.match("[A-z0-9 ]*"+name+"[A-z0-9 ]*"),Profesor.facultades == facultad if facultad else {})
         return total
-    
+
     async def create_profesor(self, profesor: Profesor):
+        profesor.id = None
         profesor.nombre = profesor.nombre.upper()
         return await self.engine.save(profesor)
-    
+
+    async def update_profesor(self, profesor: Profesor):
+        if not profesor.id:
+            raise ValueError("Profesor ID is required for update")
+        profesordb: Profesor | None = await self.get_profesor(profesor.id)
+        if not profesordb:
+            raise ValueError("Profesor not found")
+        profesordb.model_update(profesor)
+        profesordb.nombre = profesor.nombre.upper()
+
+        return await self.engine.save(profesor)
+
     async def delete_profesor(self, profesor_id):
         try:
             #await self.engine.remove(Notas, Notas.profesor == profesor_id)
@@ -57,7 +70,7 @@ class ProfesorRepository:
             return True
         except:
             return False
-    
+
     async def get_profesor_score(self, profesor_id):
         pipeline = []
         pipeline.extend(match("profesor", profesor_id, regex=False))
@@ -71,7 +84,7 @@ class ProfesorRepository:
         pipeline = []
         pipeline.extend(match("profesor", profesor_id, False))
         pipeline.extend(NOTAS_PROMEDIO)
-        
+
         collection = self.engine.get_collection(Notas)
-        
+
         return await collection.aggregate(pipeline).to_list(length=None)
